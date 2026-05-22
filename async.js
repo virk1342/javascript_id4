613a88c9683da70b9fd358c3b63465ea2d6071a3
virk1342 <tarmanvirk0001@gmail.com>
2026-05-22 00:02:22 +0000

    start

---
+++ async.js
@@ -0,0 +1,11 @@
+//async means , 
+//things that happen in sync
+//like conolse.log("onn")
+//conosle.log("two")
+//these will come up lkike
+//1
+//2
+
+//so happening in sync
+
+//nut we sometimes need to move to next function or whatever , we cant stay stuck on something,
